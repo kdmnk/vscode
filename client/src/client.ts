696b27a1be772ffc91ae4f92f8ba3a5f953764a2
Katkó Dominik <56202545+kdmnk@users.noreply.github.com>
2021-12-27 21:18:06 +0100

    generalised, add variable name

--- client/src/client.ts
+++ client/src/client.ts
@@ -10,6 +10,9 @@ import {
 
 let client: LanguageClient;
 
+let commandsAddAtom:Array<string> = ["rename-fun", "rename-mod", "extract-fun", "copy-mod"];
+let commandsAddVar:Array<string> = ["generalise-fun"];
+
 export async function get_client(context: ExtensionContext): Promise<LanguageClient> {
     let clientOptions: LanguageClientOptions = {
         documentSelector: [{ scheme: 'file', language: 'erlang' }],
@@ -21,13 +24,31 @@ export async function get_client(context: ExtensionContext): Promise<LanguageCli
         },
         middleware: {
 			executeCommand: async (command, args, next) => {
-                if(command == "rename-fun" || command == "rename-mod" || command == "extract-fun") {
+                let commandName : string = command.split(':')[1];
+                if(commandsAddAtom.includes(commandName)) {
                     const selected = await Window.showInputBox({placeHolder:"New name", validateInput: (value) => {
                         if (value.length < 1) {
                             return "Name must be at least 1 character long";
                         }
                         if (!/^[a-z][\_a-zA-Z0-9\@]*$/.test(value)) { //TODO quoted atoms
                             return "Name must be a valid atom"; 
+                        }
+                        return null;
+                    }});
+                    if (selected === undefined) {
+                        return next(command, args);
+                    }
+                    args = args.slice(0);
+                    args.push(selected);
+                    
+                }
+                if(commandsAddVar.includes(commandName)) {
+                    const selected = await Window.showInputBox({placeHolder:"New name", validateInput: (value) => {
+                        if (value.length < 1) {
+                            return "Name must be at least 1 character long";
+                        }
+                        if (!/^[A-Z][\_a-zA-Z0-9\@]*$/.test(value)) { //TODO check
+                            return "Name must be a valid variable name"; 
                         }
                         return null;
                     }});
